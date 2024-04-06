383abd5f5aa86a7239ac171a963f8ea7b61dfce2
MengYueqi <mengyq2002@gmail.com>
2024-04-06 17:13:26 +0800

    add symbol list basic func, framework of semantic

--- lab_2/semantic.c
+++ lab_2/semantic.c
@@ -2,10 +2,99 @@
 # include "semantic.h"
 # include "parser.h"
 
+// 设定头指针
+symbol_list head = NULL;
+
+// 创建变量表
+symbol_list _createSymbolList(){
+    symbol_list pointer = (symbol_list)malloc(sizeof(struct _SymbolList));
+    pointer->name = (char*)malloc(sizeof(35 * sizeof(char)));
+    pointer->type = NULL;
+    pointer->next = NULL;
+    return pointer;
+}
+
+// 初始化变量表
+void _initSymbolList(){
+    head = _createSymbolList();
+}
+
+// 打印变量表
+void _printSymbolList(symbol_list head){
+    head = head->next;
+    while (head){
+        printf("Symbol: %s, Type: %s", head->name, head->type);
+        head = head->next;
+    }    
+}
+
+// 向符号表中添加记录
+void _addRecord(symbol_list sym_record){
+    // 头插法添加
+    sym_record->next = head->next;
+    head->next = sym_record;
+}
+
+// 对 ExtDef 的分析
+void _ExtDef(Node root){
+
+}
+
+// 对 Def 的分析
+void _Def(Node root){
+
+}
+
+// 对 Exp 的分析
+void _Exp(Node root){
+    /* Exp 对应的语法
+    Exp -> Exp ASSIGNOP Exp
+         | Exp AND Exp
+         | Exp OR Exp
+         | Exp RELOP Exp
+         | Exp PLUS Exp
+         | Exp MINUS Exp
+         | Exp STAR Exp
+         | Exp DIV Exp
+         | LP Exp RP
+         | MINUS Exp
+         | NOT Exp
+         | ID LP Args RP
+         | ID LP RP
+         | Exp LB Exp RB
+         | Exp DOT ID
+         | ID
+         | INT
+         | FLOAT
+         */
+    
+    // 分析第 0 个 child
+    Node child_0 = root->child[0];
+    // 如果是 ID, INT, FLOAT
+    if (!strcmp(child_0->name, "INT")){
+
+    } else if (!strcmp(child_0->name, "FLOAT")){
+        printf("%f", child_0->FLOAT_NUM);
+    } else if (!strcmp(child_0->name, "ID")){
+        /* code */
+    }
+}
+
 void semantic(Node root){
     if (root == NULL){
         return;
     }
+
+    // 根据不同的产生式部分进行分析 
+    if (!strcmp(root->name, "ExtDef")){
+        _ExtDef(root);
+    } else if (!strcmp(root->name, "Def")){
+        _Def(root);
+    } else if (!strcmp(root->name, "Exp")){
+        _Exp(root); 
+    }
+    for (int i = 0; i < root->num_child; i++){
+        semantic(root->child[i]);
+    }
     
-    printf("%s", root->name);
 }
--- lab_2/semantic.h
+++ lab_2/semantic.h
@@ -4,7 +4,30 @@
 # include "parser.h"
 
 void semantic(Node root);
+typedef struct _SymbolList *symbol_list;
+typedef enum {BASIC, ARRAY, STRUCTURE} Kind;  // 三种类型数据
+typedef enum {INT, FLOAT} basic_type;  // 基本类型
+typedef struct _Type *type;  // 类型指针
 
+// 声明一个数据的类型
+struct _Type{
+    Kind kind;
+    union data{
+        // 基本的数据类型
+        basic_type basic;
+        // 数组类型
+        struct {type arr_type; int size;} arr;
+        // 结构体类型
+        symbol_list struct_pointer;
+    };
+};
+
+// 变量单元指针
+struct _SymbolList{
+    char *name;
+    type type;
+    symbol_list next;
+};
 
 
 #endif
